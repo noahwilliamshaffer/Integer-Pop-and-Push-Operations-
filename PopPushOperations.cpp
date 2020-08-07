8845298b6b7c2adbdd4c4463ac4f1da5e405f4c0
Noah William Shaffer <67817365+noahwilliamshaffer@users.noreply.github.com>
2020-08-07 15:53:15 -0400

    Update PopPushOperations.cpp

--- PopPushOperations.cpp
+++ PopPushOperations.cpp
@@ -5,3 +5,9 @@ x /= 10;
 //push operation:
 temp = rev * 10 + pop;
 rev = temp;
+
+//However, this approach is dangerous, because the statement \text{temp} = \text{rev} \cdot 10 + \text{pop}temp=rev⋅10+pop can cause overflow.
+
+//Luckily, it is easy to check beforehand whether or this statement would cause an overflow.
+
+//To explain, lets assume that \text{rev}rev is positive.
